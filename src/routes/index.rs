f499734003151f65a0ae070e50ed4bb033943011
angrynode <angrynode@kl.netlib.re>
2025-11-14 15:05:04 +0100

    feat: Add IndexTemplate constructor and remove unused fields

--- src/routes/index.rs
+++ src/routes/index.rs
@@ -1,7 +1,6 @@
 use askama::Template;
 use askama_web::WebTemplate;
 use axum::extract::State;
-use axum::response::{IntoResponse, Response};
 use snafu::prelude::*;
 
 // TUTORIAL: https://github.com/SeaQL/sea-orm/blob/master/examples/axum_example/
@@ -9,43 +8,38 @@ use crate::database::category::CategoryOperator;
 use crate::extractors::user::User;
 use crate::state::{AppState, AppStateContext, error::*};
 
-use std::collections::HashMap;
-
 #[derive(Template, WebTemplate)]
 #[template(path = "index.html")]
 pub struct IndexTemplate {
     /// Global application state (errors/warnings)
     pub state: AppStateContext,
-    /// TODO: Submitted values in a POST request
-    ///
-    /// This happens when the request was rejected by the handler, but still
-    /// wants to repopulate form data from submitted values.
-    pub post: HashMap<String, String>,
     /// Logged-in user.
     pub user: Option<User>,
     /// Categories
     pub categories: Vec<String>,
 }
 
+impl IndexTemplate {
+    pub async fn new(app_state: AppState, user: Option<User>) -> Result<Self, AppStateError> {
+        let categories: Vec<String> = CategoryOperator::new(app_state.clone(), user.clone())
+            .list()
+            .await
+            .context(CategorySnafu)?
+            .into_iter()
+            .map(|x| x.name)
+            .collect();
+
+        Ok(IndexTemplate {
+            state: app_state.context().await?,
+            user,
+            categories,
+        })
+    }
+}
+
 pub async fn index(
     State(app_state): State<AppState>,
     user: Option<User>,
-) -> Result<Response, AppStateError> {
-    let app_state_context = app_state.context().await?;
-
-    let categories: Vec<String> = CategoryOperator::new(app_state.clone(), user.clone())
-        .list()
-        .await
-        .context(CategorySnafu)?
-        .into_iter()
-        .map(|x| x.name)
-        .collect();
-
-    Ok(IndexTemplate {
-        state: app_state_context,
-        post: HashMap::new(),
-        user,
-        categories,
-    }
-    .into_response())
+) -> Result<IndexTemplate, AppStateError> {
+    IndexTemplate::new(app_state, user).await
 }
