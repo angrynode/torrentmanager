4972b890989c96d6eac9c9a6edfd9f9a275d14f9
amateurforger <amateurforger@kl.netlib.re>
2026-02-17 13:14:17 +0100

    minor: Move AppStateContext to dedicated module

---
+++ src/state/context.rs
@@ -0,0 +1,30 @@
+use axum::extract::{FromRequestParts, OptionalFromRequestParts};
+use axum::http::request::Parts;
+
+use super::*;
+
+/// Basic templating context used across pages.
+///
+/// Loading it may fail for some reasons, but it's so rare
+/// and unrecoverable that it will trigger a global error
+/// by rendering the AppStateError into an axum Response.
+pub struct AppStateContext {
+    pub errors: Vec<AppStateError>,
+    pub free_space: FreeSpace,
+    pub user: Option<User>,
+}
+
+impl FromRequestParts<AppState> for AppStateContext {
+    type Rejection = AppStateError;
+
+    async fn from_request_parts(
+        parts: &mut Parts,
+        state: &AppState,
+    ) -> Result<Self, Self::Rejection> {
+        Ok(Self {
+            errors: vec![],
+            free_space: state.free_space()?,
+            user: User::from_request_parts(parts, state).await?,
+        })
+    }
+}
--- src/state/mod.rs
+++ src/state/mod.rs
@@ -1,5 +1,3 @@
-use axum::extract::{FromRequestParts, OptionalFromRequestParts};
-use axum::http::request::Parts;
 use hightorrent_api::hightorrent::{SingleTarget, TorrentContent, TorrentList};
 use hightorrent_api::{Api, QBittorrentClient};
 use sea_orm::*;
@@ -9,6 +7,8 @@ use crate::config::AppConfig;
 use crate::extractors::user::User;
 use crate::migration::{Migrator, MigratorTrait};
 
+mod context;
+pub use context::AppStateContext;
 pub mod error;
 pub mod flash_message;
 pub mod free_space;
@@ -35,32 +35,6 @@ pub struct AppState {
 
     // TODO: multiple torrent backends
     pub torrent_client: QBittorrentClient,
-}
-
-/// Basic templating context used across pages.
-///
-/// Loading it may fail for some reasons, but it's so rare
-/// and unrecoverable that it will trigger a global error
-/// by rendering the AppStateError into an axum Response.
-pub struct AppStateContext {
-    pub errors: Vec<AppStateError>,
-    pub free_space: FreeSpace,
-    pub user: Option<User>,
-}
-
-impl FromRequestParts<AppState> for AppStateContext {
-    type Rejection = AppStateError;
-
-    async fn from_request_parts(
-        parts: &mut Parts,
-        state: &AppState,
-    ) -> Result<Self, Self::Rejection> {
-        Ok(Self {
-            errors: vec![],
-            free_space: state.free_space()?,
-            user: User::from_request_parts(parts, state).await?,
-        })
-    }
 }
 
 impl AppState {
